26f86643bfdf73b7762df32c1bfa126661deda80
Ludwig Schubert <git@ludwigschubert.de>
2020-09-03 09:39:42 +0200

    Various CSS resilience fixes and style cleanups

--- src/diagrams/tuning-curves.svelte
+++ src/diagrams/tuning-curves.svelte
@@ -4,21 +4,21 @@
 </script>
 
 <style>
-  /* ul {
-    padding: 0;
-  }
-  li {
-    display: flex;
-    margin: 0.5em 0;
-    max-height: 80px;
-  } */
-  div {
+  div.grid {
     display: grid;
-    grid-template-columns: 1fr 8fr;
+
+    /* 1:6 is the aspect ratio of response image in 2nd column. 
+       This should keep feature vis images mostly square. */
+    grid-template-columns: 1fr 6fr;
+    grid-template-rows: min-content;
     grid-column-gap: 0.5em;
+    grid-row-gap: 0.5em;
+    gap: 0.5em;
   }
   .legend {
     margin-bottom: 0;
+    min-width: 0;
+    min-height: 0;
   }
 
   a.feature-link {
@@ -26,25 +26,21 @@
     display: flex;
   }
 
-  /* img.response {
-    border-radius: 2px;
-  } */
   img.feature-vis {
+    height: 100%;
     object-fit: none;
     object-position: center;
     border-radius: 0.25rem;
   }
-  img.response {
-    width: 100%;
-    height: 100%;
-  }
-
-  /* .unit-row {
-    max-height: 64px;
-  } */
 
   svg {
     align-self: center;
+    justify-self: end;
+    overflow: visible;
+  }
+
+  svg#tuning-curves-f-ratio {
+    max-height: 86px;
   }
 
   svg .range {
@@ -63,8 +59,10 @@
   }
 </style>
 
-<div class="legend">
-  <svg width="20px" height="100%" style="justify-self: end;">
+<div class="grid">
+  <!-- we'd like to set height=100% here, but Safari renders that wrongly -->
+  <!-- 35px is roughly the smallest the synth stimuli image should get on mobile and looks ok-ish -->
+  <svg id="tuning-curves-f-ratio" width="40px" height="35px">
     <defs>
       <marker
         id="t2"
@@ -81,15 +79,16 @@
     </defs>
     <g class="range">
       <line
+        hidden="hidden"
         x1="50%"
-        y1="calc(0% + 1px)"
+        y1="0%"
         x2="50%"
-        y2="calc(100% - 1px)"
+        y2="100%"
         marker-end="url(#t2)"
         marker-start="url(#t2)" />
       <text
         class="axis-label"
-        transform="rotate(-90)"
+        transform=""
         transform-origin="center"
         x="50%"
         y="50%"
@@ -99,20 +98,20 @@
       </text>
     </g>
   </svg>
+
   <img
+    style="width=100%;"
     src="diagrams/1.4-tuning-curves/samples-small.png"
     alt="A sampled grid from the two axes of variation of our synthetic stimuli:
     orientation and spatial frequency ratio." />
-</div>
 
-<div class="legend">
   <span />
 
   <svg width="100%" height="20px">
     <defs>
       <marker
         id="t"
-        markerWidth="2"
+        markerWidth="3"
         markerHeight="10"
         refX="0"
         refY="5"
@@ -125,9 +124,9 @@
     </defs>
     <g class="range">
       <line
-        x1="calc(0% + 1px)"
+        x1="0%"
         y1="50%"
-        x2="calc(100% - 1px)"
+        x2="100%"
         y2="50%"
         marker-end="url(#t)"
         marker-start="url(#t)" />
@@ -141,10 +140,8 @@
       </text>
     </g>
   </svg>
-</div>
 
-{#each units as unit, index}
-  <div class="unit-row">
+  {#each units as unit, index}
     <a
       class="feature-link"
       href="https://storage.googleapis.com/inceptionv1-weight-explorer/mixed3a_{unit.index}.html">
@@ -160,26 +157,25 @@
       alt={`Responses of unit ${unit.index}.`}
       title={`Responses of unit ${unit.index}.`} />
     <!-- <span class="figcaption unit">
-      <code>{layer}</code>
-      Unit {unit.index}
-    </span>
-    <svg width="100%" height="10px" viewbox="0 0 360 10">
-      <g class="range">
-        <line x1="1" y1="0" x2="1" y2="10" />
-        <line x1="1" y1="5" x2="200" y2="5" />
-        <line x1="200" y1="0" x2="200" y2="10" />
-      </g>
-    </svg> -->
-  </div>
-{/each}
+    <code>{layer}</code>
+    Unit {unit.index}
+  </span>
+  <svg width="100%" height="10px" viewbox="0 0 360 10">
+    <g class="range">
+      <line x1="1" y1="0" x2="1" y2="10" />
+      <line x1="1" y1="5" x2="200" y2="5" />
+      <line x1="200" y1="0" x2="200" y2="10" />
+    </g>
+  </svg> -->
+  {/each}
+</div>
 
 <figcaption class="figcaption">
   <a href="#figure-3" class="figure-number">3</a>
   : Responses of six high-low frequency detectors to artifical stimuli. The
   stimuli vary over two axes: orientation, ranging from 0 to 2π, and frequency
-  ratio, ranging from 1:1 to 1:10. Activation values are normalized across all
-  units shown, ranging from 0 to 1.
-  <span
-    class="viridis"
-    style="display: inline; margin-left: 5px; padding-right: 64px;" />
+  ratio ƒ, ranging from 1:1 to 1:10. Activation values are normalized across all
+  units shown, ranging between 0
+  <span class="color-legend viridis" />
+  and 1.
 </figcaption>
